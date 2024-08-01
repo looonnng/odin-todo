e97851e1461a09affa1d62130b4b0e2ae2b79490
T14s <122837733+looonnng@users.noreply.github.com>
2024-07-31 22:52:00 -0500

    feat: add fn to create top part of todo card

--- src/loadTodo.js
+++ src/loadTodo.js
@@ -5,4 +5,26 @@ const createMyElement = (tag, classList = [], text = '') => {
   return element;
 };
 
-export const createTask = (task) => {};
+export const createTask = (task) => {
+  const todoCard = createMyElement('div', ['todos-card', 'col', 'my-task']);
+  const todoCardTop = createTodoCardTop();
+  
+  todoCard.append(todoCardTop);
+};
+
+function createTodoCardTop() {
+  const todoCardTop = createMyElement('div', ['todos-card__top', 'row']);
+  const todoCardName = createMyElement('div', ['todos-card__name'], 'My Task');
+  const todoCardMoreOptionBtn = createMyElement('div', [
+    'todos-card-list-options-btn',
+  ]);
+  const moreVertIcon = createMyElement(
+    'span',
+    ['material-symbols-outlined', 'size-20'],
+    'more_vert',
+  );
+
+  todoCardMoreOptionBtn.appendChild(moreVertIcon);
+  todoCardTop.append(todoCardName, todoCardMoreOptionBtn);
+  return todoCardTop;
+}
