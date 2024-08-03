446197fc150860141ff9d08479262b12a6466ef0
T14s <122837733+looonnng@users.noreply.github.com>
2024-08-03 18:24:41 -0500

    refactor: update function name

--- src/loadTodo.js
+++ src/loadTodo.js
@@ -1,3 +1,4 @@
+// Utility function
 const createMyElement = (tag, classList = [], text = '') => {
   const element = document.createElement(tag);
   element.classList.add(...classList);
@@ -5,7 +6,12 @@ const createMyElement = (tag, classList = [], text = '') => {
   return element;
 };
 
-export const createTaskCard = (task) => {
+// Create new task list and task container
+export const createTaskList = () => {
+  const taskListWrapper = createMyElement('div', [
+    'task-lists-container__wrapper',
+    'row',
+  ]);
   const todoCard = createMyElement('div', ['todos-card', 'col', 'my-task']);
   const todoCardTop = createTodoCardTop();
   const addTaskButton = createButton(
@@ -17,11 +23,12 @@ export const createTaskCard = (task) => {
 
   addTaskButton.appendChild(createMyElement('p', [], 'Add a task'));
   todoCard.append(todoCardTop, addTaskButton, myTaskContainer);
+  taskListWrapper.appendChild(todoCard);
 
-  return todoCard;
+  return taskListWrapper;
 };
 
-// Todo Card Top
+// Task List Top
 function createTodoCardTop() {
   const todoCardTop = createMyElement('div', ['todos-card__top', 'row']);
   const todoCardName = createMyElement('div', ['todos-card__name'], 'My Task');
@@ -35,7 +42,7 @@ function createTodoCardTop() {
   return todoCardTop;
 }
 
-// Todo Card
+// Utility function
 function createButton(
   buttonClassList = [],
   iconName,
@@ -49,24 +56,26 @@ function createButton(
   return button;
 }
 
+// Task Container
 function createTaskContainer() {
   const taskContainer = createMyElement('div', ['todos-card__tasks-container']);
   return taskContainer;
 }
 
-function createTodo(task) {
+// Todo item
+export function createTodo(task) {
   const myTask = createMyElement('div', ['task', 'row']);
   const doneBtnWrapper = createMyElement('div', ['done-btn-wrapper']);
   const doneBtn = createButton(['done-btn'], 'circle', []);
   const taskTodo = createMyElement('div', ['task__todo', 'row']);
   const todoText = createMyElement('p', ['task__todo-text'], task);
-  const moreOptions = createButton(
+  const moreOptionsBtn = createButton(
     ['task__todo-more-options', 'more-options-btn'],
     'more_vert',
     ['size-20'],
   );
 
-  taskTodo.append(todoText, moreOptions);
+  taskTodo.append(todoText, moreOptionsBtn);
   doneBtnWrapper.appendChild(doneBtn);
   myTask.append(doneBtnWrapper, taskTodo);
 
