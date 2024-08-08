7c443fe8449dcd97477c1ef8ffde7cf3a46b1452
T14s <122837733+looonnng@users.noreply.github.com>
2024-08-07 21:16:06 -0500

    feat: add stylings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 import '@material-design-icons/font';
 import modalModule from './modal';
+import loadTaskListToSideBar from './loadNewList';
 
 modalModule();
+loadTaskListToSideBar();
