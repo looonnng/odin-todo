f428dce4496d41b116aa4bac4b4ae847bc7b916d
T14s <122837733+looonnng@users.noreply.github.com>
2024-08-13 21:52:20 -0500

    feat: init default list using IIFE

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import './style.css';
 import '@material-design-icons/font';
 import modalModule from './modal';
+import { createTaskList, createTodo } from './loadTodo';
+import { createMyElement } from './loadTodo';
+
+// Init default lists
+(() => {
+  const myTaskList = createTaskList('My Task');
+  const myTask = createTodo('lorem');
+
+  // Side menu
+  const checkBox = createMyElement('input', ['side-menu__checkbox']);
+  const checkBoxLabel = createMyElement(
+    'label',
+    ['side-menu__item'],
+    'My Task',
+  );
+
+  checkBox.type = 'checkbox';
+  checkBox.id = 'My Task';
+  checkBoxLabel.htmlFor = 'My Task';
+  checkBoxLabel.prepend(checkBox);
+
+  document.querySelector('.task-lists-container').appendChild(myTaskList);
+  document.querySelector('[data-task-container="My Task"]').appendChild(myTask);
+  document.querySelector('.list-container').appendChild(checkBoxLabel);
+})();
+
+// Init modal
 
 modalModule();
