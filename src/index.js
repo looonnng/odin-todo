6d117f208488daee7d086822e8588cff29375b5c
T14s <122837733+looonnng@users.noreply.github.com>
2024-08-14 01:00:51 -0500

    feat: render local storage refactor: init default list using JSON

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
 import './style.css';
 import '@material-design-icons/font';
 import modalModule from './modal';
-import { createTaskList, createTodo } from './loadTodo';
-import { createMyElement } from './loadTodo';
+import { createMyElement, createTodo, createTaskList } from './loadTodo';
 
-// Init default lists
+// Init default lists && local storage
 (() => {
-  const myTaskList = createTaskList('My Task');
-  const myTask = createTodo('lorem');
+  const myTaskListObject = {
+    projectTitle: 'My Task',
+    projectTasks: ['lorem'],
+  };
 
-  // Side menu
-  const checkBox = createMyElement('input', ['side-menu__checkbox']);
-  const checkBoxLabel = createMyElement(
-    'label',
-    ['side-menu__item'],
-    'My Task',
-  );
-
-  checkBox.type = 'checkbox';
-  checkBox.id = 'My Task';
-  checkBoxLabel.htmlFor = 'My Task';
-  checkBoxLabel.prepend(checkBox);
-
-  document.querySelector('.task-lists-container').appendChild(myTaskList);
-  document.querySelector('[data-task-container="My Task"]').appendChild(myTask);
-  document.querySelector('.list-container').appendChild(checkBoxLabel);
+  localStorage.setItem('My Task', JSON.stringify(myTaskListObject));
 })();
 
 // Init modal
-
 modalModule();
+
+(function populateStorage() {
+  for (let i = 0; i < localStorage.length; i++) {
+    const projectString = localStorage.key(i);
+    const projectObject = JSON.parse(localStorage.getItem(projectString));
+    const projectTasksArray = projectObject.projectTasks;
+    const projectListTitle = projectObject.projectTitle;
+
+    const myTaskList = createTaskList(projectListTitle);
+    const checkBox = createMyElement('input', ['side-menu__checkbox']);
+    const checkBoxLabel = createMyElement(
+      'label',
+      ['side-menu__item'],
+      projectListTitle,
+    );
+
+    checkBox.type = 'checkbox';
+    checkBox.id = projectListTitle;
+    checkBoxLabel.htmlFor = projectListTitle;
+    checkBoxLabel.prepend(checkBox);
+
+    document.querySelector('.task-lists-container').appendChild(myTaskList);
+    document.querySelector('.list-container').appendChild(checkBoxLabel);
+
+    projectTasksArray.forEach((task) => {
+      const myTask = createTodo(task);
+      document
+        .querySelector(`[data-task-container="${projectListTitle}"]`)
+        .appendChild(myTask);
+    });
+  }
+})();
